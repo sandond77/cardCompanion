20f3f1d3f9b6b105cd9abfaa83e69336f01e2cd2
sandond77 <sandond77@gmail.com>
2025-11-11 11:50:24 -0800

    added missing cors lines to server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,17 @@ dotenv.config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			'http://localhost:3000',
+			'http://localhost:3001',
+			'http://localhost:5173',
+			'https://cardcompanion.onrender.com'
+		],
+		credentials: true
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //allows express to parse json)
 
